8b8d1ab769a968aabf75b831b29728414501fc4b
Milan Oberkirch <milan@oberkirch.org>
2024-11-23 23:23:18 +0100

    Replace wrapper by drop-in fallback

--- blink.py
+++ blink.py
@@ -1,63 +1,64 @@
-import time
 import random
+import time
+from collections.abc import Callable, Iterable
 from functools import cache
-from typing import Callable, Iterable, NoReturn, Self, Sequence, SupportsIndex, overload
-from dataclasses import dataclass
+from shutil import get_terminal_size
+from typing import NamedTuple, Protocol, Self, SupportsIndex, cast, overload
 
-from rpi_ws281x import Color, PixelStrip as LedStrip, ws
-
-RESET_ANSI = '\x1b[0m'
+from rpi_ws281x import Color, PixelStrip
 
 
-
-class Strip:
-    led_strip: LedStrip | list[int]
-    num: int
-
-    @overload
-    def __init__(self, *, led_strip: LedStrip): ...
-    @overload
-    def __init__(self, *, num: int) -> None: ...
-    def __init__(self, *, num: int | None = None, led_strip: LedStrip | None = None):
-        if led_strip:
-            self.led_strip = led_strip
-            self.num = led_strip.numPixels()
-        elif num:
-            self.led_strip = [0] * num
-            self.num = num
-        else:
-            raise TypeError
-
+class Strip(Protocol):
     @overload
     def __setitem__(self, pos: SupportsIndex, value: int, /) -> None: ...
     @overload
     def __setitem__(self, pos: slice, value: Iterable[int], /) -> None: ...
-    def __setitem__(self, pos, value, /) -> None:
-        self.led_strip[pos] = value
-    
+
     @overload
-    def __getitem__(self, pos: SupportsIndex) -> int: ...
+    def __getitem__(self, pos: SupportsIndex, /) -> int: ...
     @overload
-    def __getitem__(self, pos: slice) -> Iterable[int]: ...
-    def __getitem__(self, pos):
-        return self.led_strip[pos]
+    def __getitem__(self, pos: slice, /) -> Iterable[int]: ...
 
-    def __len__(self):
-        return self.num
+    def __len__(self) -> int: ...
 
-    def show(self):
-        if isinstance(self.led_strip, LedStrip):
-            self.led_strip.show()
-        else:
-            pixels = (
-                f"{RGB.from_int(value).ansi}▪"
-                for value in self
-            )
-            print(f"\x1b[48;2;0;0;0m{''.join(pixels)}{RESET_ANSI}", flush=True)
+    def show(self) -> None: ...
 
 
-@dataclass(slots=True, eq=True)
-class RGB:
+class TerminalStrip(list[int]):
+    """Drop-in replacement for PixelStrip printing pixels to the terminal"""
+
+    def __init__(self, num: int, *_args, **_kwargs):
+        super().__init__([0] * num)
+
+    def show(self) -> None:
+        pixels = (f"{RGB.from_int(value).ansi}▪" for value in self)
+        print(f"\x1b[48;2;0;0;0m{''.join(pixels)}\x1b[0m", flush=True)
+
+
+@cache
+def get_strip(num: int | None = None, pin: int = 18) -> Strip:
+    """Try to initialize real PixelStrip, fall back to terminal output"""
+    try:
+        strip = PixelStrip(
+            num=num or 300,
+            pin=pin,
+        )
+        strip.begin()
+        # the cast is half a lie: PixelStrip _technically_ allows using kwargs
+        # for `__getitem__` etc. and has no static annotations guaranteeing the
+        # the relationship between pos and return type (int->int, slice->list:
+        return cast(Strip, strip)
+    except RuntimeError:
+        return TerminalStrip(num=num or get_terminal_size().columns)
+
+
+class HSI(NamedTuple):
+    hue: float
+    saturation: float
+    intensity: float
+
+
+class RGB(NamedTuple):
     r: int
     g: int
     b: int
@@ -65,21 +66,22 @@ class RGB:
     @classmethod
     def from_int(cls, r: int) -> Self:
         return cls(
-            r=(r >> 16) & 0xff,
-            g=(r >> 8) & 0xff,
-            b=r & 0xff,
+            r=(r >> 16) & 0xFF,
+            g=(r >> 8) & 0xFF,
+            b=r & 0xFF,
         )
 
     @classmethod
     def random(cls) -> Self:
-        return cls.from_int(random.randint(0, 0xffffff))
+        return cls.from_int(random.randint(0, 0xFFFFFF))
 
     @property
-    def ansi(self):
+    def ansi(self) -> str:
         return f"\x1b[38;2;{self.r};{self.g};{self.b}m"
 
     @property
-    def hsi(self) -> tuple[float, float, float]:
+    def to_hsi(self) -> HSI:
+        # see https://en.wikipedia.org/wiki/HSL_and_HSV
         max_ = max(self.r, self.g, self.b)
         min_ = min(self.r, self.g, self.b)
         intensity = (self.r + self.g + self.b) / 3
@@ -96,58 +98,37 @@ class RGB:
                     raise AssertionError("max of items is one of items")
         else:
             hue = 0.0
-        return hue, saturation, intensity
-
+        return HSI(hue, saturation, intensity)
 
     def __str__(self) -> str:
         return f"#{int(self):06x}"
 
     def __invert__(self) -> Self:
-        return self.from_int(0xffffff - int(self))
+        return self.from_int(0xFFFFFF - int(self))
 
     def __int__(self) -> int:
         return (self.r << 16) | (self.g << 8) | self.b
 
-    def __lt__(self, other: Self) -> bool:
-        return self.hsi < other.hsi
-
-
-@cache
-def get_strip() -> Strip:
-    try:
-        led_strip = LedStrip(
-            num=300,
-            pin=18,
-            freq_hz=800_000,
-            dma=10,
-            brightness=20,
-            invert=False,
-            channel=0,
-            strip_type=ws.WS2811_STRIP_GRB,
-        )
-        led_strip.begin()
-        return Strip(led_strip=led_strip)
-    except RuntimeError:
-        return Strip(num=200)
-
 
 def wipe(strip: Strip, color: int = Color(0, 0, 0)) -> None:
-    for i in range(strip.num):
+    for i in range(len(strip)):
         strip[i] = color
     strip.show()
 
 
-def random_rain(strip: Strip, pixels: list[int] | None = None, sleep: float = 0.001) -> None:
-    positions = list(range(0, len(strip)))
+def random_rain(
+    strip: Strip, pixels: list[int] | None = None, sleep: float = 0.001
+) -> None:
+    positions = list(range(len(strip)))
     random.shuffle(positions)
     for pos in positions:
         strip[pos] = pixels[pos] if pixels else int(RGB.random())
         strip.show()
-        time.sleep(0.001)
+        time.sleep(sleep)
 
 
 def random_wipe(strip: Strip, c: int = 0) -> None:
-    pixels = [x for x in range(0, strip.num)]
+    pixels = [x for x in range(len(strip))]
     random.shuffle(pixels)
     for i in pixels:
         strip[i] = c
@@ -156,32 +137,39 @@ def random_wipe(strip: Strip, c: int = 0) -> None:
 
 
 def shuffle(strip: Strip) -> None:
-    strip[:] = [
-        int(RGB.random())
-        for _ in range(len(strip))
-    ]
+    strip[:] = [int(RGB.random()) for _ in range(len(strip))]
     strip.show()
 
 
-def _close_in(from_, to):
+def _close_in(from_: int, to: int) -> int:
     if from_ == to:
         return to
     return from_ + (1 if from_ < to else -1)
 
 
-def slow_transition(strip: Strip, c: RGB | None = None, c_next: RGB  | None = None) -> None:
+def slow_transition(
+    strip: Strip, c: RGB | None = None, c_next: RGB | None = None
+) -> None:
     c = c or RGB.random()
     c_next = c_next or ~c
     wipe(strip, int(c))
     while c != c_next:
-        c.r = _close_in(c.r, c_next.r)
-        c.g = _close_in(c.g, c_next.g)
-        c.b = _close_in(c.b, c_next.b)
+        c = RGB(
+            _close_in(c.r, c_next.r),
+            _close_in(c.g, c_next.g),
+            _close_in(c.b, c_next.b),
+        )
         wipe(strip, int(c))
         time.sleep(0.01)
 
 
-def quicksort(strip: Strip, lt_func: Callable[[RGB, RGB], bool] = RGB.__lt__, sleep: float = 0.01, from_index: int = 0, to_index: int | None = None):
+def quicksort(
+    strip: Strip,
+    lt_func: Callable[[RGB, RGB], bool] = lambda a, b: a.to_hsi < b.to_hsi,
+    sleep: float = 0.01,
+    from_index: int = 0,
+    to_index: int | None = None,
+) -> None:
     """Sort the colors on the strip by their numerical value using quicksort"""
     to_index = len(strip) - 1 if to_index is None else to_index
     if from_index >= to_index:
@@ -205,36 +193,19 @@ def quicksort(strip: Strip, lt_func: Callable[[RGB, RGB], bool] = RGB.__lt__, sl
     quicksort(strip, lt_func=lt_func, sleep=sleep, from_index=i, to_index=to_index)
 
 
-def hsi(strip):
-    strip[:] = map(int, sorted(
-        RGB.from_int(i * (0xffffff // len(strip)))
-        for i in range(len(strip))
-    )
-    )
-    strip.show()
-
-
-def rgb(strip):
-    strip[:] = (
-        i * (0xffffff // len(strip))
-        for i in range(len(strip))
-    )
-    strip.show()
-
-
-def pride(strip):
+def pride(strip: Strip) -> None:
     new_pride_colors = [
-        0xffffff,
-        0xf6aab8,
-        0x60cdf6,
+        0xFFFFFF,
+        0xF6AAB8,
+        0x60CDF6,
         0x674018,
         0x050708,
-        0xe51e25,
-        0xf68d1f,
-        0xf9ee14,
-        0x0d8040,
-        0x3d5fac,
-        0x742a85,
+        0xE51E25,
+        0xF68D1F,
+        0xF9EE14,
+        0x0D8040,
+        0x3D5FAC,
+        0x742A85,
     ]
     pixels = [
         new_pride_colors[int(len(new_pride_colors) * (i / len(strip)))]
@@ -246,20 +217,30 @@ def pride(strip):
     quicksort(strip, lambda x, y: False)
     random.shuffle(pixels)
     random_rain(strip, pixels)
-    quicksort(strip, lambda x, y: new_pride_colors.index(int(x)) < new_pride_colors.index(int(y)), sleep=0.1)
+    quicksort(
+        strip,
+        lambda x, y: new_pride_colors.index(int(x)) < new_pride_colors.index(int(y)),
+        sleep=0.1,
+    )
     time.sleep(1)
     for c in new_pride_colors:
         random_wipe(strip, c)
     c = RGB.from_int(new_pride_colors[-1])
-    for c_next in new_pride_colors:
-        slow_transition(strip, c, RGB.from_int(c_next))
+    for c_next in map(RGB.from_int, new_pride_colors):
+        slow_transition(strip, c, c_next)
+        c = c_next
     random.shuffle(pixels)
     random_rain(strip, pixels)
-    quicksort(strip, lambda x, y: new_pride_colors.index(int(x)) < new_pride_colors.index(int(y)))
+    quicksort(
+        strip,
+        lambda x, y: new_pride_colors.index(int(x)) < new_pride_colors.index(int(y)),
+    )
 
 
-def all_the_colors(strip):
-    random_rain(strip, pixels=[i * (0xffffff // len(strip)) for i in range(len(strip), 0, -1)])
+def all_the_colors(strip: Strip) -> None:
+    random_rain(
+        strip, pixels=[i * (0xFFFFFF // len(strip)) for i in range(len(strip), 0, -1)]
+    )
     quicksort(strip, lambda x, y: int(x) < int(y))
     time.sleep(1)
     quicksort(strip)
